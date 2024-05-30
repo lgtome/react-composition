2ddcdd9d397e665e6f2fb5a6294894a68fb76ac6
lgtome <seryikotenok232@gmail.com>
2024-05-30 23:38:10 +0300

    fix: wrapper component fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { CompositionContextProvider } from './Composition/Context'
 import { CompositionLayout } from './Composition/CompositionLayout'
 import { Composition } from './Composition'
 
 export const App = () => {
     return (
         <>
-            <CompositionContextProvider>
-                <CompositionLayout />
-            </CompositionContextProvider>
+            <CompositionLayout />
         </>
     )
 }
--- src/Composition/Wrapper.tsx
+++ src/Composition/Wrapper.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FCWithChildren } from './types'
+import { CompositionContextProvider } from './Context'
 
 export const Wrapper: FCWithChildren<unknown> = ({ children }) => {
-    return children
+    return <CompositionContextProvider>{children}</CompositionContextProvider>
 }
